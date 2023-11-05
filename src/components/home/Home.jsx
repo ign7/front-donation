6ab48f659e798b7c0d2f2346fea3d0c7aa001f80
igor <quaresmaigor@yahoo.com>
2023-11-05 10:59:12 -0300

    autethication and style nav

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,45 +15,45 @@ function Home() {
 
     window.addEventListener('beforeunload', () => {
         localStorage.clear(); // Limpa todo o conteúdo do localStorage
-      });
+    });
 
     const navigate = useNavigate();
 
     const [listadonation, setlistadonation] = useState([]);
 
-    
+
 
     function getAllDonations() {
         if (selectedOption === 'TODOS') {
-          axios.get('http://localhost:8080/donations/todos', {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`, // Inclua o token aqui
-            },
-          }).then(data => {
-            console.log(data);
-            console.log(localStorage.getItem('tokenjwt'));
-            setlistadonation(data.data);
-          }).catch(error => {
-            alert('NÃO AUTORIZADO.');
-            console.log(error);
-          });
+            axios.get('http://localhost:8080/donations/todos', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`, // Inclua o token aqui
+                },
+            }).then(data => {
+                console.log(data);
+                console.log(localStorage.getItem('tokenjwt'));
+                setlistadonation(data.data);
+            }).catch(error => {
+                alert('NÃO AUTORIZADO.');
+                console.log(error);
+            });
         }
-      }
+    }
 
     function getDonationByCategoria(option) {
         if (option !== 'TODOS') {
-          axios.get('http://localhost:8080/donations/pesquisardoacao/categoriadoacao=' + option, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`, // Inclua o token aqui
-            },
-          }).then(data => {
-            console.log(data);
-            console.log(localStorage.getItem('tokenjwt'));
-            setlistadonation(data.data);
-          }).catch(error => {
-            alert('NÃO AUTORIZADO.');
-            console.log(error);            
-          });
+            axios.get('http://localhost:8080/donations/pesquisardoacao/categoriadoacao=' + option, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`, // Inclua o token aqui
+                },
+            }).then(data => {
+                console.log(data);
+                console.log(localStorage.getItem('tokenjwt'));
+                setlistadonation(data.data);
+            }).catch(error => {
+                alert('NÃO AUTORIZADO.');
+                console.log(error);
+            });
         }
     }
 
@@ -96,7 +96,7 @@ function Home() {
     }
 
     const renderPageNumbers = pageNumbers.map((number) => (
-        
+
         <button key={number} onClick={() => setCurrentPage(number)}>
             {number}
         </button>
@@ -109,12 +109,47 @@ function Home() {
         });
     }
 
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isDoador, setIsDoador] = useState(true);
+
+    useEffect(() => {
+        if (isLoading) {
+            const login = localStorage.getItem('login');
+            axios.get(`http://localhost:8080/usuarios/login=${login}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+                },
+            })
+                .then(response => {
+                    setUserData(response.data); // Armazena os dados do usuário no estado local
+                    setIsLoading(false); // Indica que os dados do usuário foram carregados
+                    if (response.data.role === 'DOADOR') {
+                        setIsDoador(false);
+                    }
+                })
+                .catch(error => {
+                    alert('NÃO AUTORIZADO.');
+                    console.log(error);
+                    setIsLoading(false); // Indica que ocorreu um erro ao carregar os dados do usuário
+                });
+        }
+    }, [isLoading, isDoador]);
+
 
     return (
         <div className="Home">
             <div class="homepage">
-
                 <div className='nav-bar-home'>
+                    <div className='label-navs-user'>
+                        {isLoading ? (
+                            <p>Carregando dados do usuário...</p>
+                        ) : (
+                            userData && <p className='hover:text-white'><span class="material-symbols-outlined">
+                                person
+                            </span>Olá,{userData.nome}!</p>
+                        )}
+                    </div>
                     <div class="">
                         <div className='atividades'>
                             <h1>Atividades do Sistema</h1>
@@ -125,14 +160,19 @@ function Home() {
                         <div class="" id='container-nav-actions'>
                             <ul>
                                 <div className='label-navs'>
-                                    <li class="mb-2">
-                                        <Link to="/teladoacao" class="hover:text-white">
-                                            <span class="material-symbols-outlined">
-                                                app_registration
-                                            </span>
-                                        </Link>
-                                        <p>Cadastrar Doaçoes</p>
-                                    </li>
+                                    {isDoador ? (
+                                        <p></p>
+                                    ) : (
+                                        <li class="mb-2">
+                                            <Link to="/teladoacao" class="hover:text-white">
+                                                <span class="material-symbols-outlined">
+                                                    app_registration
+                                                </span>
+                                            </Link>
+                                            <p>Cadastrar Doaçoes</p>
+                                        </li>
+                                    )}
+
                                 </div>
 
                                 <div className='label-navs'>
@@ -190,18 +230,18 @@ function Home() {
                     <div className='titulo-homepage'>
                         <div className='content-educ'>
                             <div>
-                            <h1 class="text-6xl font-bold">Educ. Sustentável System <span class="material-symbols-outlined">
-                                park
-                            </span></h1>
+                                <h1 class="text-6xl font-bold">Educ. Sustentável System <span class="material-symbols-outlined">
+                                    park
+                                </span></h1>
                             </div>
                             <div>
-                            <span class=''>
-                            <h2 className='h2-educ'>
-                                Somos o "Projeto educação Sustentavel" acreditamos que a melhor forma de mudar as pessoas é através da bondade da leitura e do conhecimento. 
-                                Gerenciamos as doações para que os materiais recebidos sejam destinados a novas pessoas que necessitam deste material afim de estudos.
-                                Realizamos a intermediação entre DOADOR e RECEPTOR na triagem dos materiais.
-                            </h2>
-                            </span>
+                                <span class=''>
+                                    <h2 className='h2-educ'>
+                                        Somos o "Projeto educação Sustentavel" acreditamos que a melhor forma de mudar as pessoas é através da bondade da leitura e do conhecimento.
+                                        Gerenciamos as doações para que os materiais recebidos sejam destinados a novas pessoas que necessitam deste material afim de estudos.
+                                        Realizamos a intermediação entre DOADOR e RECEPTOR na triagem dos materiais.
+                                    </h2>
+                                </span>
                             </div>
                         </div>
 
