b2c1e210592f91023c9a16a312c13b85c08e1e9f
igor <quaresmaigor@yahoo.com>
2023-11-17 20:39:00 -0300

    aceitar e recusar solicitaçao

--- src/components/solicitacoes/SolicitacaoRecebida.jsx
+++ src/components/solicitacoes/SolicitacaoRecebida.jsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import './../donation/Donation.css'
+import { Button, Modal, Datepicker } from 'flowbite-react';
+
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+
+
+import './Solicitacao.css'
 
 
 
@@ -13,114 +18,179 @@ function SolicitacaoRecebida() {
 
   const [userData, setUserData] = useState(null);
 
-   useEffect(() => {
+  const [solicitacaoData, setsolicitacao] = useState(null);
 
-            const login = localStorage.getItem('login');
-            axios.get(`http://localhost:8080/usuarios/login=${login}`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
-                },
-            })
-                .then(response => {
-                    localStorage.setItem('usuarioauteticado', response.data.id);
-                    console.log(response.data.solicitacoesRecebidas);
-                    setlistaSolicitacao(response.data.solicitacoesRecebidas);
-                    setUserData(response.data); // Armazena os dados do usuário no estado local
-                })
-                .catch(error => {
-                    alert('NÃO AUTORIZADO.');
-                    console.log(error);
-                    
-                });
+  const [openModal, setOpenModal] = useState(false);
 
-}, []) 
+  useEffect(() => {
+
+    const login = localStorage.getItem('login');
+    axios.get(`http://localhost:8080/usuarios/login=${login}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+      },
+    })
+      .then(response => {
+        console.log(response.data);
+        setlistaSolicitacao(response.data.doacoes);
+        setUserData(response.data); // Armazena os dados do usuário no estado local
+
+      })
+      .catch(error => {
+        alert('NÃO AUTORIZADO.');
+        console.log(error);
+
+      });
+
+  }, [])
+
+
+  function confirmar() {
+    const idsol = solicitacaoData.solicitacao.id;
+    const iddonation = solicitacaoData.id_donation;
+    const receptorid = solicitacaoData.id_receptor;
+    const doador_id = solicitacaoData.id_doador;
+
+    axios.patch(
+      `http://localhost:8080/solicitacoes/acepptSolicitation/solicitacaoid=${idsol}/doadorid=${doador_id}/receptorid=${receptorid}/donationid=${iddonation}`,
+      null, // Passando null como corpo da requisição
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+        },
+      }
+    )
+      .then(msg => {
+        alert(msg.data);
+        localStorage.setItem('id_usuario_vencedor',receptorid);
+        localStorage.setItem('solicitacaoaceita',msg.data);
+        setOpenModal(false);
+      })
+      .catch(error => {
+        alert(error);
+        setOpenModal(false);
+        console.log(error);
+      });
+  }
+
+  function recusar() {
+    const idsol = solicitacaoData.solicitacao.id;
+
+    axios.patch(
+      `http://localhost:8080/solicitacoes/rejeitarSolicitation/id=${idsol}`,
+      null, // Passando null como corpo da requisição
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+        },
+      }
+    )
+      .then(msg => {
+        alert(msg.data);
+        setOpenModal(false);
+      })
+      .catch(error => {
+        alert(error);
+        setOpenModal(false);
+        console.log(error);
+      });
+  }
+
+  function telaconfirmação(idsolicitacao) {
+    console.log(idsolicitacao);
+    axios.get('http://localhost:8080/solicitacoes/solicitacaoporid/byid=' + idsolicitacao, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+      },
+    })
+      .then(msg => {
+        setOpenModal(true)
+        console.log(msg.data)
+        setsolicitacao(msg.data);
+
+      }).catch(error => {
+
+        console.log(error);
+      });
+  }
 
   return (
-    <div className="CadastrarDonation">
-            <div class="Donationpage">
-                <div className='container-donationView '>
+    <div className="CadastrarDonation bg-green-300 min-h-screen flex justify-center items-center">
+      <div className="bg-white p-8 shadow-md rounded-lg w-full max-w-2xl">
 
-                    <div className='container-conteudo-sol'>
-                        <div className='title-donation'>
-                            <h1 class="text-4xl font-bold font-serif pt-4 transition-transform transform hover:scale-95 ">nomeDonation</h1>
-                            <p className=''>Informaçoes sobre a Doação.</p>
+        <div className="container-donationView">
+
+          <div className="container-conteudo-sol">
+
+            <div className="title-donation text-center">
+              <h1 className="text-4xl font-bold font-serif mb-4 transition-transform transform hover:scale-95">Solicitações Recebidas</h1>
+              <p className="text-gray-600">Solicitações Recebidas de suas Doações</p>
+            </div>
+
+            {listaSolicitacao.map((doacao) => (
+              <div key={doacao.id} className="card--sol transition-transform transform hover:scale-95 bg-gray-200 p-4 mb-4 rounded-md">
+                <div className="">
+                  <div className="border-b mb-4 pb-2">
+                    <h1 className="font-bold text-xl mb-2">Doação: {doacao.nome}</h1>
+                  </div>
+
+                  {doacao.donationSolicitadas && doacao.donationSolicitadas.map((solicitacao) => (
+                    <div key={solicitacao.id}>
+                      <div className="conteudo">
+                        <p className="font-bold text-xl mb-2">Data: {solicitacao.dataSolicitacao}</p>
+                        <div className="flex mb-2">
+                          <span className="inline-block bg-yellow-300 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2">{solicitacao.role}</span>
                         </div>
+                        <div className="mb-4">
+                          <label className="font-semibold">Descrição:</label>
+                          <p className="text-gray-700">{solicitacao.observacao}</p>
+                        </div>
+                      </div>
 
-                        {listaSolicitacao.map((donation) => (
-                            <div key={donation.id} className="card--sol transition-transform transform hover:scale-105">
-                                <div className="">
-                                    <hr />
-                                    <div className="titulocard">
-                                        <h1 class="font-bold text-xl mb-2" >{donation.nome}</h1>
-                                        <hr />
-                                    </div>
-                                    <div className='conteudo'>
-                                        <p className="font-bold text-xl mb-2">
-                                            {donation.dataDoacao}
-                                        </p>
-                                        <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">{donation.status}</span>
-                                        <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white">{donation.categoria}</span>
-                                    </div>
-                                </div>
-
-                                <div className="div-btn">
-                                    <button disabled className="btn-info" >
-                                        <span class="material-symbols-outlined">
-                                            settings
-                                        </span>
-                                        <div>
-                                            <p>Configuraçoes</p>
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
+                      <div className="flex justify-between items-center mt-4">
+                        <button disabled className="btn-info ">
+                          <span className="material-symbols-outlined">settings</span>
+                          <div>
+                            <p>Configurações</p>
+                          </div>
+                        </button>
+                        <button onClick={() => telaconfirmação(solicitacao.id)} className="bg-green-500 text-white py-2 px-4 rounded-md ml-2">Aceitar</button>
+                        <button disabled className="bg-gray-500 text-white py-2 px-4 rounded-md ml-2">Disabled</button>
+                      </div>
                     </div>
-
-                    
+                  ))}
                 </div>
-
-                {/* <div className='container-material'>
-
-                    <div className='material'>
-                        <h1 className='text-2xl font-bold  font-serif p-4 pl-6'>Materias Doação nomeDonation</h1>
-                        
-                            <div  className="card--material transition-transform transform hover:scale-105">                               
-                                    <div  className="">
-                                        <hr />
-                                        <div className="titulocard">
-                                            <h1 className="font-bold text-xl mb-2">donation.nome</h1>
-                                            <hr />
-                                        </div>
-                                        <div className='conteudo'>
-                                            <p className="font-bold text-xl mb-2">
-                                                donation.dataSolicitacao
-                                            </p>
-                                            <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">donation.status</span>
-                                            <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white">donation.categoria</span>
-                                        </div>
-                                        <div className="div-btn">
-                                            <button disabled className="btn-info" >
-                                                <span className="material-symbols-outlined">
-                                                    settings
-                                                </span>
-                                                <div>
-                                                    <p>Configurações</p>
-                                                </div>
-                                            </button>
-                                        </div>
-                                    </div>
-                               
-                            </div>
-                       
-                    </div>
-                </div> */}
-
-            </div>
-            <div >
-            </div>
+              </div>
+            ))}
+          </div>
 
         </div>
+      </div>
+
+      <Modal show={openModal} size="md" onClose={() => setOpenModal(false)} popup>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-green-500 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-green-700 dark:text-gray-400">
+              Sua solicitação será enviada para o doador, apos a confirmação !!
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="success" onClick={() => confirmar()}>
+                {"Aceitar"}
+              </Button>
+              <Button color="gray" onClick={() => recusar()}>
+                Recusar
+              </Button>
+            </div>
+          </div>
+
+        </Modal.Body>
+      </Modal>
+    </div>
+
+
+
   );
 }
 
--- src/components/solicitacoes/SolicitacoesEnviadas.jsx
+++ src/components/solicitacoes/SolicitacoesEnviadas.jsx
@@ -2,126 +2,91 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-import './../donation/Donation.css'
+
+import './Solicitacao.css'
 
 
 
 function SolicitacaoEnviada() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [listaSolicitacao, setlistaSolicitacao] = useState([]);
+    const [listaSolicitacao, setlistaSolicitacao] = useState([]);
 
-  const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState(null);
 
-   useEffect(() => {
+    useEffect(() => {
 
-            const login = localStorage.getItem('login');
-            axios.get(`http://localhost:8080/usuarios/login=${login}`, {
-                headers: {
-                    Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
-                },
+        const login = localStorage.getItem('login');
+        axios.get(`http://localhost:8080/usuarios/login=${login}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('tokenjwt')}`,
+            },
+        })
+            .then(response => {
+                localStorage.setItem('usuarioauteticado', response.data.id);
+                console.log(response.data.solicitacoesRecebidas);
+                setlistaSolicitacao(response.data.solicitacoesEnviadas);
+                setUserData(response.data); // Armazena os dados do usuário no estado local
             })
-                .then(response => {
-                    localStorage.setItem('usuarioauteticado', response.data.id);
-                    console.log(response.data.solicitacoesRecebidas);
-                    setlistaSolicitacao(response.data.solicitacoesEnviadas);
-                    setUserData(response.data); // Armazena os dados do usuário no estado local
-                })
-                .catch(error => {
-                    alert('NÃO AUTORIZADO.');
-                    console.log(error);
-                    
-                });
+            .catch(error => {
+                alert('NÃO AUTORIZADO.');
+                console.log(error);
 
-}, []) 
+            });
 
-  return (
-    <div className="CadastrarDonation">
-            <div class="Donationpage">
-                <div className='container-donationView '>
+    }, [])
 
-                    <div className='container-conteudo-sol'>
-                        <div className='title-donation'>
-                            <h1 class="text-4xl font-bold font-serif pt-4 transition-transform transform hover:scale-95 ">nomeDonation</h1>
-                            <p className=''>Informaçoes sobre a Doação.</p>
-                        </div>
-
-                        {listaSolicitacao.map((donation) => (
-                            <div key={donation.id} className="card--sol transition-transform transform hover:scale-105">
-                                <div className="">
-                                    <hr />
-                                    <div className="titulocard">
-                                        <h1 class="font-bold text-xl mb-2" >{donation.nome}</h1>
-                                        <hr />
-                                    </div>
-                                    <div className='conteudo'>
-                                        <p className="font-bold text-xl mb-2">
-                                            {donation.dataDoacao}
-                                        </p>
-                                        <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">{donation.status}</span>
-                                        <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white">{donation.categoria}</span>
-                                    </div>
-                                </div>
-
-                                <div className="div-btn">
-                                    <button disabled className="btn-info" >
-                                        <span class="material-symbols-outlined">
-                                            settings
-                                        </span>
-                                        <div>
-                                            <p>Configuraçoes</p>
-                                        </div>
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
+    return (
+        <div className="CadastrarDonation bg-green-300 min-h-screen flex justify-center items-center">
+        <div className="bg-white p-8 shadow-md rounded-lg w-full max-w-2xl">
+      
+          <div className="container-donationView">
+      
+            <div className="container-conteudo-sol">
+      
+              <div className="title-donation text-center">
+                <h1 className="text-4xl font-bold font-serif mb-4 transition-transform transform hover:scale-95">Solicitações Enviadas</h1>
+                <p className="text-gray-600">Solicitações Enviadas</p>
+              </div>
+      
+              {listaSolicitacao.map((solicitacao) => (
+                <div key={solicitacao.id} className="card--sol transition-transform transform hover:scale-105 bg-gray-200 p-4 mb-4 rounded-md">
+                  <div className="border-b mb-4 pb-2">
+                    <h1 className="font-bold text-xl mb-2">{solicitacao.nome}</h1>
+                  </div>
+                  <div className="conteudo">
+                    <p className="font-bold text-xl mb-2">{solicitacao.dataSolicitacao}</p>
+                    <div className="flex mb-2">
+                      <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">{solicitacao.role}</span>
+                      <span className="inline-block">{/* Espaço vazio entre spans */}</span>
+                      <span className="inline-block">{/* Adicione estilos específicos para a descrição */}</span>
                     </div>
-
-                    
+                    <div className="mb-4">
+                      <label className="font-semibold">Descrição:</label>
+                      <p className="text-gray-700">{solicitacao.observacao}</p>
+                    </div>
+                  </div>
+      
+                  <div className="flex justify-between items-center mt-4">
+                    <button disabled className="btn-info ">
+                      <span className="material-symbols-outlined">settings</span>
+                      <div>
+                        <p>Configurações</p>
+                      </div>
+                    </button>
+      
+                    <button className="bg-green-500 text-white py-2 px-4 rounded-md ml-2">Aceitar </button>
+                    <button className="bg-red-500 text-white py-2 px-4 rounded-md ml-2">Recusar</button>
+                  </div>
+      
                 </div>
-
-                {/* <div className='container-material'>
-
-                    <div className='material'>
-                        <h1 className='text-2xl font-bold  font-serif p-4 pl-6'>Materias Doação nomeDonation</h1>
-                        
-                            <div  className="card--material transition-transform transform hover:scale-105">                               
-                                    <div  className="">
-                                        <hr />
-                                        <div className="titulocard">
-                                            <h1 className="font-bold text-xl mb-2">donation.nome</h1>
-                                            <hr />
-                                        </div>
-                                        <div className='conteudo'>
-                                            <p className="font-bold text-xl mb-2">
-                                                donation.dataSolicitacao
-                                            </p>
-                                            <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">donation.status</span>
-                                            <span className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-white">donation.categoria</span>
-                                        </div>
-                                        <div className="div-btn">
-                                            <button disabled className="btn-info" >
-                                                <span className="material-symbols-outlined">
-                                                    settings
-                                                </span>
-                                                <div>
-                                                    <p>Configurações</p>
-                                                </div>
-                                            </button>
-                                        </div>
-                                    </div>
-                               
-                            </div>
-                       
-                    </div>
-                </div> */}
-
+              ))}
             </div>
-            <div >
-            </div>
-
+      
+          </div>
         </div>
-  );
+      </div>
+    );
 }
 
 export default SolicitacaoEnviada;
